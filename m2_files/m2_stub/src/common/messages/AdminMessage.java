c8a7be583ee4fd75022b8f6b9c93a1d0818433c8
kenliao94 <kenliao94@gmail.com>
2018-02-23 21:44:31 -0500

    add AdminMessage Interface and operation message

---
+++ m2_files/m2_stub/src/common/messages/AdminMessage.java
@@ -0,0 +1,31 @@
+package common.messages;
+
+public interface AdminMessage {
+    
+    public enum OptType {
+        INIT,
+        START,
+        STOP,
+        ADD,
+        REMOVE,
+    }
+
+    public enum OptStatus {
+        INIT_SUCCESS,
+        INIT_ERROR,
+        START_SUCCESS,
+        START_ERROR,
+        STOP_SUCCESS,
+        STOP_ERROR,
+        ADD_SUCCESS,
+        ADD_ERROR,
+        REMOVE_SUCCESS,
+        REMOVE_ERROR
+    }
+
+    public OptType getOptType();
+
+    public OptStatus getOptStatus();
+}
+
+
