f25ff8d28657b961f8c2c9f4adea5f23574235b0
Liao  Qi Ran <liaoqi1@ug173.eecg.toronto.edu>
2018-03-10 18:10:48 -0500

    Setup unit testing infra

---
+++ m2_files/m2_stub/src/testing/ECSTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import java.net.UnknownHostException;
+
+import client.KVStore;
+
+import junit.framework.TestCase;
+
+
+public class ECSTest extends TestCase {
+
+	public void testConnectionSuccess() {
+		Exception ex = null;		
+		assertNull(ex);
+	}
+}
