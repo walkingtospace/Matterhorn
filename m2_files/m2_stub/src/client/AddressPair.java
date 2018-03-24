02c78520f067f5e2deb4e29b50a2f7cfee5cc09c
DavidWang2012 <david.wang2012@live.com>
2018-03-23 20:14:31 -0400

    Added most Crash Detection Logic

--- m2_files/m2_stub/src/client/AddressPair.java
+++ m2_files/m2_stub/src/client/AddressPair.java
@@ -2,20 +2,28 @@ package client;
 
 import java.util.Objects;
 
-public class AddressKey {
+public class AddressPair {
     private final String address;
     private final int port;
 
-    public AddressKey(String address, int port) {
+    public AddressPair(String address, int port) {
         this.address = address;
         this.port = port;
+    }
+    
+    public String getHost() {
+    	return this.address;
+    }
+    
+    public int getPort() {
+    	return this.port;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof AddressKey)) return false;
-        AddressKey otherKey = (AddressKey) o;
+        if (!(o instanceof AddressPair)) return false;
+        AddressPair otherKey = (AddressPair) o;
         return address == otherKey.address && port == otherKey.port;
     }
 
