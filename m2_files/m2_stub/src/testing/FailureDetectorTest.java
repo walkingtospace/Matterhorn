476439844276ca4565546363a97a9884e137648a
DavidWang2012 <david.wang2012@live.com>
2018-03-24 21:29:29 -0400

    Revert "asdf"
    This reverts commit 5a8b40fdc8396f376c62ced5812c2b78604dfd7e.

--- m2_files/m2_stub/src/testing/FailureDetectorTest.java
+++ m2_files/m2_stub/src/testing/FailureDetectorTest.java
@@ -2,17 +2,15 @@ package testing;
 
 import org.junit.Test;
 
-import failure_detector.FailureDetector;
 import junit.framework.TestCase;
 
 public class FailureDetectorTest extends TestCase {
-	/*private FailureDetector failureDetector;
+	private FailureDetector failureDetector;
 	
 	@Test
 	public void testUnion() {
-		int intervalSeconds = 30;
-		String zkHostname = "0.0.0.0";
-		int zkPort = 3200;
-		FailureDetector failureDetector = new FailureDetector(intervalSeconds, zkHostname, zkPort);
-	}*/
+		
+	}
+	
+	
 }
